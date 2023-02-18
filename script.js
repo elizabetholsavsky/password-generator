e7035db86cd8b3739356dcf8438d39f3bd7482f2
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-02-18 14:39:46 -0600

    added comments and psuedocode to style and js sheets

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get references to the #generate element (button)
 var generateBtn = document.querySelector("#generate");
+
+// Generate password function
+
+// Prompt password criteria; length 8-128
+// Character types: lowercase, uppercase, numbers, special chars
+
+// Validate input
+
+// Generate and display password
+
+// Display generated password
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // Displays password on page
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button (button click)
 generateBtn.addEventListener("click", writePassword);
